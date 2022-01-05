aaf04bcbd0d398f5d16d27bcb414a985b47785b7
Matt Jewell <m@mattjewell.co.uk>
2022-01-05 15:08:13 +0100

    allow Readable as input to the read function

--- __tests__/unit/read.spec.ts
+++ __tests__/unit/read.spec.ts
@@ -1,7 +1,29 @@
-import * as fs from 'fs';
+import {createReadStream, readFileSync} from 'fs';
 import {read} from '../../src';
 
 declare const require: (path: string) => any;
+
+
+describe('#read', () => {
+    describe.each([
+        ['ABN AMRO', 'abn-amro-1.STA', 'abn-amro-1.json'],
+        ['ING-1', 'ing-1.mta', 'ing-1.json'],
+        ['ING-2', 'ing-2.mta', 'ing-2.json'],
+        ['BASE-1', 'base-1.mta', 'base-1.json'],
+        ['BASE-2', 'base-2.mta', 'base-2.json'],
+        ['BASE-3', 'base-3.mta', 'base-3.json']
+    ])('Provider: %s', (_, mt940FileName, resultFileName) => {
+        describe.each(['Buffer', 'ArrayBuffer', 'ReadStream'])('%s', (type) => {
+            it('should parse the file content', async () => {
+                const data = getTestData(type as any, mt940FileName);
+                const expectedResult = require(`./../cases/${resultFileName}`);
+
+                expect(await read(data)).toEqual(expectedResult);
+            });
+        });
+    });
+});
+
 
 function toArrayBuffer(buffer: Buffer) {
     const length: number = buffer.length;
@@ -15,24 +37,16 @@ function toArrayBuffer(buffer: Buffer) {
     return ab;
 }
 
-describe('#read', () => {
-    describe.each([
-        ['ABN AMRO', 'abn-amro-1.STA', 'abn-amro-1.json'],
-        ['ING-1', 'ing-1.mta', 'ing-1.json'],
-        ['ING-2', 'ing-2.mta', 'ing-2.json'],
-        ['BASE-1', 'base-1.mta', 'base-1.json'],
-        ['BASE-2', 'base-2.mta', 'base-2.json'],
-        ['BASE-3', 'base-3.mta', 'base-3.json']
-    ])('Provider: %s', (_, mt940FileName, resultFileName) => {
-        describe.each(['Buffer', 'ArrayBuffer'])('%s', (type) => {
-            it('should parse the file content', async () => {
-                const buffer: Buffer = fs.readFileSync(`./__tests__/cases/${mt940FileName}`);
-                const expectedResult: any[] = require(`./../cases/${resultFileName}`);
-
-                const data = type === 'Buffer' ? buffer : toArrayBuffer(buffer);
-
-                expect(await read(data)).toEqual(expectedResult);
-            });
-        });
-    });
-});
+function getTestData(dataType: 'ReadStream' | 'Buffer' | 'ArrayBuffer', mt940FileName: string) {
+    switch (dataType) {
+        case 'Buffer':
+            return readFileSync(`./__tests__/cases/${mt940FileName}`);
+        case 'ReadStream':
+            return createReadStream(`./__tests__/cases/${mt940FileName}`);
+        case 'ArrayBuffer':
+            return toArrayBuffer(readFileSync(`./__tests__/cases/${mt940FileName}`));
+        default: {
+            throw new Error(`unrecognised type ${dataType}`);
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import md5 from './utils/md5';
 import * as parser from './parser';
+import type {Readable} from 'stream';
 
 const invalidInputMessage: string = 'invalid input';
 
@@ -8,6 +9,7 @@ export interface Tag {
     open?: (state: State) => void;
     readContent?: (state: State, symbolCode: number) => void;
     close?: (state: State, options: ReadOptions) => void;
+
     readToken(state: State): number;
 }
 
@@ -61,10 +63,17 @@ export interface ReadOptions {
     getTransactionId(transaction: Transaction, index: number): string;
 }
 
-export function read(input: ArrayBuffer | Buffer, options?: ReadOptions): Promise<Statement[]> {
+
+export async function read(input: Readable | ArrayBuffer | Buffer, options?: ReadOptions): Promise<Statement[]> {
     let data: Uint8Array | Buffer;
 
-    if (typeof Buffer !== 'undefined' && input instanceof Buffer) {
+    if (isReadStream(input)) {
+        const chunks = [];
+        for await (const chunk of input) {
+            chunks.push(chunk);
+        }
+        data = Buffer.concat(chunks);
+    } else if (typeof Buffer !== 'undefined' && input instanceof Buffer) {
         data = input;
     } else if (typeof ArrayBuffer !== 'undefined' && input instanceof ArrayBuffer) {
         data = new Uint8Array(input);
@@ -88,3 +97,8 @@ export function read(input: ArrayBuffer | Buffer, options?: ReadOptions): Promis
             return Promise.reject(new Error(invalidInputMessage));
         });
 }
+
+
+// check for fs stream in an isomorphic safe way by Duck typing
+const isReadStream = (input: Readable | ArrayBuffer | Buffer) =>
+    '_read' in input && typeof (input as Readable)._read === 'function';
