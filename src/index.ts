9bb0b7364a384eb1b61e93d361b8c48366153774
Matt Jewell <m@mattjewell.co.uk>
2022-01-05 15:59:35 +0100

    by chunking on :20: the parser works with abn amro format as a stream

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import md5 from './utils/md5';
 import * as parser from './parser';
-import type {Readable} from 'stream';
+import type {Readable, TransformCallback} from 'stream';
+import {Transform} from 'stream';
 
 const invalidInputMessage: string = 'invalid input';
 
@@ -64,16 +65,18 @@ export interface ReadOptions {
 }
 
 
-export async function read(input: Readable | ArrayBuffer | Buffer, options?: ReadOptions): Promise<Statement[]> {
+export function read(input: Readable | ArrayBuffer | Buffer, options?: ReadOptions): Promise<Statement[]> {
+    if (isReadStream(input)) {
+        return readStream(input as Readable, options);
+    }
+
+    return readAsync(input as ArrayBuffer | Buffer, options);
+}
+
+export function readAsync(input: ArrayBuffer | Buffer, options?: ReadOptions): Promise<Statement[]> {
     let data: Uint8Array | Buffer;
 
-    if (isReadStream(input)) {
-        const chunks = [];
-        for await (const chunk of input) {
-            chunks.push(chunk);
-        }
-        data = Buffer.concat(chunks);
-    } else if (typeof Buffer !== 'undefined' && input instanceof Buffer) {
+    if (typeof Buffer !== 'undefined' && input instanceof Buffer) {
         data = input;
     } else if (typeof ArrayBuffer !== 'undefined' && input instanceof ArrayBuffer) {
         data = new Uint8Array(input);
@@ -102,3 +105,68 @@ export async function read(input: Readable | ArrayBuffer | Buffer, options?: Rea
 // check for fs stream in an isomorphic safe way by Duck typing
 const isReadStream = (input: Readable | ArrayBuffer | Buffer) =>
     '_read' in input && typeof (input as Readable)._read === 'function';
+
+
+async function readStream(input: Readable, options?: ReadOptions): Promise<Statement[]> {
+    let statements: Statement[] = [];
+    for await (const chunk of input.pipe(splitBy(':20:'))) {
+        statements = statements.concat(await readAsync(chunk, options));
+    }
+    return statements;
+}
+
+
+export const splitBy = (separator: string): Transform => {
+    let bufferedChunks: string[] = [];
+
+    return new Transform({
+        flush(callback: TransformCallback) {
+            return callback(null, bufferedChunks.join(''));
+        },
+        transform(chunk: any, _encoding: string, callback: TransformCallback) {
+            try {
+                if (chunk == null) {
+                    this.push(null);
+                    return;
+                }
+
+                if (chunk instanceof Buffer) {
+                    chunk = chunk.toString();
+                }
+
+                if (typeof chunk !== 'string') {
+                    return callback(
+                        new Error('invalid data type, only strings and buffer streams are acceptable')
+                    );
+                }
+
+                if (chunk.includes(separator)) {
+                    // Concat the first part to the existing chunks
+                    const [head, ...tail] = chunk.split(separator);
+
+
+                    bufferedChunks.push(head);
+                    const newLine = bufferedChunks.join('');
+                    bufferedChunks = [separator];
+                    this.push(newLine);
+
+
+                    // Send all new lines, except last one
+                    // Last element may be partial
+                    const last = tail.pop();
+                    if (last) {
+                        bufferedChunks.push(last);
+                    }
+                    // pop() is a mutable method, therefore last is removed
+                    tail.forEach(line => this.push(line.concat(separator)));
+                } else {
+                    bufferedChunks.push(chunk);
+                }
+
+                return callback();
+            } catch (e) {
+                return callback(e);
+            }
+        }
+    });
+};
